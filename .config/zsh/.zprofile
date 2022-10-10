d0c17303269a5a1190f2d9312d42421c1288af55
ttl256 <64251319+ttl256@users.noreply.github.com>
2022-10-10 15:08:08 +0300

    fix

--- .config/zsh/.zprofile
+++ .config/zsh/.zprofile
@@ -4,3 +4,5 @@ type nvim &> /dev/null && editor="nvim"
 
 export EDITOR=$editor
 export VISUAL=$EDITOR
+
+type /opt/homebrew/bin/brew &> /dev/null && eval "$(/opt/homebrew/bin/brew shellenv)"
--- provision_dotfiles.sh
+++ provision_dotfiles.sh
@@ -11,7 +11,7 @@ CMD="git --git-dir=$DOTFILES_DIR --work-tree=$HOME"
 set -e
 [ -d "$DOTFILES_DIR" ] && echo "$DOTFILES_DIR already exists" && eval "$CMD pull" && exit
 git clone --separate-git-dir="$DOTFILES_DIR" https://github.com/ttl256/.dotfiles.git "$TMP_DOTFILES_DIR"
-rsync --recursive --verbose --exclude '.git' "$TMP_DOTFILES_DIR/" "$HOME/"
+rsync -a --verbose --exclude '.git' "$TMP_DOTFILES_DIR/" "$HOME/"
 rm -r "$TMP_DOTFILES_DIR"
 eval "$CMD config status.showUntrackedFiles no"
 if [ ! -d "$HOME/.config/nvim/bundle/Vundle.vim" ]; then
