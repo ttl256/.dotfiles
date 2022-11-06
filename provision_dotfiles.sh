f8afdd4b68248d47e32dce700ac2fed00d537b6d
ttl256 <64251319+ttl256@users.noreply.github.com>
2022-11-06 01:22:09 +0100

    fix

--- provision_dotfiles.sh
+++ provision_dotfiles.sh
@@ -9,13 +9,18 @@ TMP_DOTFILES_DIR="$HOME/.dotfiles.tmp"
 CMD="git --git-dir=$DOTFILES_DIR --work-tree=$HOME"
 
 set -e
-[ -d "$DOTFILES_DIR" ] && echo "$DOTFILES_DIR already exists" && eval "$CMD pull" && exit
+# [ -d "$DOTFILES_DIR" ] && echo "$DOTFILES_DIR already exists" && eval "$CMD pull" && exit
+rm -rf "$DOTFILES_DIR" 
 git clone --separate-git-dir="$DOTFILES_DIR" https://github.com/ttl256/.dotfiles.git "$TMP_DOTFILES_DIR"
 rsync -a --verbose --exclude '.git' "$TMP_DOTFILES_DIR/" "$HOME/"
 rm -r "$TMP_DOTFILES_DIR"
 eval "$CMD config status.showUntrackedFiles no"
-if [ ! -d "$HOME/.config/nvim/bundle/Vundle.vim" ]; then
-	mkdir -p "$HOME/.config/nvim/bundle/Vundle.vim"
+mkdir -p "$HOME/.config/nvim/bundle/Vundle.vim"
+if [ -d "$HOME/.config/nvim/bundle/Vundle.vim/.git" ] then
+	cd "$HOME/.config/nvim/bundle/Vundle.vim"
+	git pull
+	cd
+else
 	git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.config/nvim/bundle/Vundle.vim"
-	echo | echo | vim +PluginUpdate +qall &>/dev/null
 fi
+echo | echo | vim +PluginUpdate +qall &>/dev/null
