b67c4a64c787431c1ad3ac243423eb0567f6f3be
ttl256 <64251319+ttl256@users.noreply.github.com>
2023-04-13 23:02:08 +0200

    Fix

--- provision_dotfiles.sh
+++ provision_dotfiles.sh
@@ -9,15 +9,14 @@ TMP_DOTFILES_DIR="$HOME/.dotfiles.tmp"
 CMD="git --git-dir=$DOTFILES_DIR --work-tree=$HOME"
 
 set -e
-# [ -d "$DOTFILES_DIR" ] && echo "$DOTFILES_DIR already exists" && eval "$CMD pull" && exit
 rm -rf "$DOTFILES_DIR" 
 git clone --separate-git-dir="$DOTFILES_DIR" https://github.com/ttl256/.dotfiles.git "$TMP_DOTFILES_DIR"
 rsync -a --verbose --exclude '.git' "$TMP_DOTFILES_DIR/" "$HOME/"
 rm -r "$TMP_DOTFILES_DIR"
 eval "$CMD config status.showUntrackedFiles no"
 
-PLUGIN_MANAGER_DIR="$HOME/.config/nvim/bundle/vim-plug"
-PLUGIN_MANAGER_URL="https://github.com/junegunn/vim-plug.git"
+PLUGIN_MANAGER_DIR="$HOME/.config/nvim/bundle/Vundle.vim"
+PLUGIN_MANAGER_URL="https://github.com/VundleVim/Vundle.vim.git"
 
 mkdir -p "$PLUGIN_MANAGER_DIR"
 if [ -d "$PLUGIN_MANAGER_DIR/.git" ]
@@ -28,4 +27,4 @@ then
 else
 	git clone "$PLUGIN_MANAGER_URL" "$PLUGIN_MANAGER_DIR"
 fi
-echo | echo | vim +PluginUpdate +qall &>/dev/null
+echo | echo | vim +PlugUpdate +qall &>/dev/null
