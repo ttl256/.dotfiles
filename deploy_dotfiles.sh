cf3a138b1ee159f19984fbe73bc5ced9bddd9aa6
ttl256 <64251319+ttl256@users.noreply.github.com>
2023-04-14 15:01:22 +0200

    Fixes

--- deploy_dotfiles.sh
+++ deploy_dotfiles.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 FILE="$1"
 [ -z "$FILE" ] && echo Provide name of a file with devices && exit 1
@@ -9,5 +9,7 @@ i=0
 for device in $DEVICES; do
 	i=$((i+1))
 	echo [$i/$NUM_OF_DEVICES] $device
-	ssh -T -o "StrictHostKeyChecking=no" $device < $HOME/provision_dotfiles.sh
+	( ssh -T -o "StrictHostKeyChecking=no" $device < $HOME/provision_dotfiles.sh; echo "Done $device" ) &
 done
+wait
+echo "All done"
